ebe57ab63e81943fa3492bebf5a998c1555c755c
Aaron Brooks <xenosnowfox@gmail.com>
2019-02-16 11:38:51 +1100

    split out checks into their own private methods

--- src/main/java/com/xenosnowfox/amazonaws/lambda/router/RouterImpl.java
+++ src/main/java/com/xenosnowfox/amazonaws/lambda/router/RouterImpl.java
@@ -1,9 +1,11 @@
 package com.xenosnowfox.amazonaws.lambda.router;
 
+import com.xenosnowfox.amazonaws.lambda.http.RequestMethod;
 import com.xenosnowfox.amazonaws.lambda.request.Request;
 import com.xenosnowfox.amazonaws.lambda.response.Response;
 
 import java.util.List;
+import java.util.Set;
 import java.util.Vector;
 import java.util.regex.Pattern;
 
@@ -31,26 +33,63 @@ public class RouterImpl implements Router {
         this.routes.add(route);
     }
 
+    /**
+     * Evaluates whether the given route matches the request path.
+     *
+     * @param request
+     *      Request.
+     * @param parentContext
+     *      Route Context.
+     * @param route
+     *      Route.
+     * @return
+     *      {@code true} for a match, {@code false} otherwise.
+     */
+    private boolean routeMatchesPath(final Request request, final RouteContext parentContext, final Route route) {
+        String routePath = route.getPath();
+
+        String regexSuffix = "/?";
+        if (route.getRouteHandler() instanceof Router) {
+            regexSuffix = "(/.*)?";
+        }
+
+        return routePath == null
+                || Pattern.matches(
+                        parentContext.getPath() + routePath + regexSuffix
+                        , request.getPath()
+                );
+    }
+
+    /**
+     * Evaluates whether the given route matches the request's request method.
+     *
+     * @param request
+     *      Request.
+     * @param route
+     *      Route.
+     * @return
+     *      {@code true} for a match, {@code false} otherwise.
+     */
+    private boolean routeMatchesRequestMethod(final Request request, final Route route) {
+        Set<RequestMethod> routeRequestMethods = route.getRequestMethods();
+        return routeRequestMethods == null
+                || routeRequestMethods.size() == 0
+                || routeRequestMethods.contains(request.getRequestMethod());
+    }
+
     @Override
     public final Response handle(final RouteContext parentContext, final Request request) {
 
         for (Route route : this.routes) {
-            String routePath = route.getPath();
-
-            String regexSuffix = "/?";
-            if (route.getRouteHandler() instanceof Router) {
-                regexSuffix = "(/.*)?";
-            }
 
             // check if path matches
-            if (
-                    (routePath == null || Pattern.matches(parentContext.getPath() + routePath + regexSuffix, request.getPath()))
-                    && route.matches(request)
+            if (routeMatchesPath(request, parentContext, route)
+                    && routeMatchesRequestMethod(request, route)
             ) {
 
                 MutableRouteContext context = new MutableRouteContextImpl(parentContext);
-                if (routePath != null) {
-                    context.setPath(parentContext.getPath() + routePath);
+                if (route.getPath() != null) {
+                    context.setPath(parentContext.getPath() + route.getPath());
                 }
 
                 Response resp = route.getRouteHandler().handle(context, request);
